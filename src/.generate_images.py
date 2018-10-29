f949142796b5d4d30982da8e27bbda87bcd8596f
Lee Chun Hoe <leechunhoe01@gmail.com>
2018-10-30 00:26:14 +0800

    Split TTC files to TTF

--- src/.generate_images.py
+++ src/.generate_images.py
@@ -7,20 +7,21 @@ dir_output = "img/"
 # Define your font name here
 # font_filename = "Impact.ttf"
 # font_filename = "Arial Unicode.ttf"
-font_filename = "UnGungseo.ttf"
+# font_filename = "UnGungseo.ttf"
+font_filename = "Songti.ttc"
 
 F = fontforge.open(dir_input + font_filename)
 
-if not os.path.exists(dir_output):
-	os.mkdir(dir_output)
+# if not os.path.exists(dir_output):
+# 	os.mkdir(dir_output)
 
-dir_this_font = dir_output + os.path.splitext(font_filename)[0]
+# dir_this_font = dir_output + os.path.splitext(font_filename)[0]
 
-if os.path.exists(dir_this_font):
-	shutil.rmtree(dir_this_font)
+# if os.path.exists(dir_this_font):
+# 	shutil.rmtree(dir_this_font)
 
-os.mkdir(dir_this_font)
+# os.mkdir(dir_this_font)
 
-for name in F:
-    filename = name + ".bmp"
-    F[name].export(dir_this_font + "/" + filename)
+# for name in F:
+#     filename = name + ".bmp"
+#     F[name].export(dir_this_font + "/" + filename)
--- src/train.py
+++ src/train.py
@@ -21,8 +21,11 @@ def normalise(raw_matrix):
 	background_matrix = np.zeros((SIZE, SIZE))
 	rx = raw_matrix.shape[0]
 	ry = raw_matrix.shape[1]
-	# TODO: Put into center
-	background_matrix[0:rx, 0:ry] = raw_matrix
+	x1 = (SIZE) // 2 - (rx // 2)
+	x2 = x1 + rx
+	y1 = (SIZE) // 2 - (ry // 2)
+	y2 = y1 + ry
+	background_matrix[x1:x2, y1:y2] = raw_matrix
 	return background_matrix
 
 def get_image_vector(filename, directory):
@@ -41,10 +44,13 @@ for filename in os.listdir(_DIR_Y):
 	y = get_image_vector(filename, _DIR_Y)
 	Y.append(y)
 
-print(Y[100])
+
+
+# print(Y[0])
 
 # plt.figure()
-# plt.imshow(Y[100])
+# plt.imshow(Y[90])
 # plt.colorbar()
 # plt.grid(False)
 # plt.show()
+
